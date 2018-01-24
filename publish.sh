d51ef683df8d5af86ca14a035ce187f39e187902
sjfkai <sjfkai@163.com>
2018-01-24 17:15:52 +0800

    add travis ci

---
+++ publish.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+yarn build
+cd build
+git init
+git config --global user.name "sjfkai"
+git config --global user.email "sjfkai@163.com"
+git add .
+git commit -m "republish"
+git remote add github $GIT_REPO
+git fetch github
+git push github master:gh-pages -f
