86845163b60076b30b5370a6e2ff37459a266ac4
sjfkai <sjfkai@163.com>
2019-05-09 19:59:05 +0800

    feat: 动态加载xslx库

--- src/utils/download.js
+++ src/utils/download.js
@@ -1,11 +1,10 @@
-import XLSX from 'xlsx'
 import { saveAs } from 'file-saver'
 
-export function downloadExcel (data) {
+export async function downloadExcel (data) {
+  const XLSX = await import(/* webpackChunkName: "xlsx" */ 'xlsx')
   const workbook = XLSX.utils.book_new()
   const worksheet = XLSX.utils.aoa_to_sheet(data)
   XLSX.utils.book_append_sheet(workbook, worksheet, 'mapLocation.sjfkai.com')
-
   const wopts = { bookType:'xlsx', bookSST:false, type:'array' }
   const wbout = XLSX.write(workbook,wopts)
   saveAs(new Blob([wbout],{type: 'application/octet-stream'}), 'map-location.xlsx')
